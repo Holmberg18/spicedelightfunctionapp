395894187141fb51a1413eeeffbcc2abcaf52fd9
Holmberg18 <holmberg18@gmail.com>
2024-10-03 22:30:30 -0700

    [+/-] Create call to api management service product endpoint for test

--- src/functions/server.ts
+++ src/functions/server.ts
@@ -7,13 +7,10 @@ const credential = new DefaultAzureCredential();
 async function callApiManagement() {
     // Acquire token for API Management
     const tokenResponse = await credential.getToken("https://management.azure.com/.default");
+    console.log(tokenResponse)
 
     // Make HTTP call to the API Management endpoint
-    const response = await axios.get("https://api-management-spicedelightappapi.azure-api.net/spicedelightappapi/api/Product", {
-        headers: {
-            Authorization: `Bearer ${tokenResponse.token}`
-        }
-    });
+    const response = await axios.get("https://api-management-spicedelightappapi.azure-api.net/spicedelightappapi/api/Product")
 
     console.log(response.data);
 }
@@ -22,7 +19,7 @@ export async function server(request: HttpRequest, context: InvocationContext): 
     context.log(`Http function processed request for url "${request.url}"`);
 
     const name = request.query.get('name') || await request.text() || 'world';
-    // await callApiManagement()
+    await callApiManagement()
 
     return { body: `Hello, ${name}!` };
 };
