a9a254a01819e748676794c3f6ff5e0bdde20fc9
Holmberg18 <holmberg18@gmail.com>
2024-10-03 23:49:54 -0700

    [+/-] Add product endpoint to azure function

--- src/functions/server.ts
+++ src/functions/server.ts
@@ -1,25 +1,27 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
-const { DefaultAzureCredential } = require("@azure/identity");
-const axios = require("axios");
+import { DefaultAzureCredential} from "@azure/identity"
+import axios from "axios"
 
-const credential = new DefaultAzureCredential();
 
-async function callApiManagement() {
-    // Acquire token for API Management
-    const tokenResponse = await credential.getToken("https://management.azure.com/.default");
-    console.log(tokenResponse)
+export async function callApiManagement(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    const credential = new DefaultAzureCredential();
+    const tokenResponse = await credential.getToken("api://b456166d-fe5c-43bd-a65f-de5be868f821");
+    
+    const token = tokenResponse.token;
 
-    // Make HTTP call to the API Management endpoint
-    const response = await axios.get("https://api-management-spicedelightappapi.azure-api.net/spicedelightappapi/api/Product")
+    const response = await axios.get("https://api-management-spicedelightappapi.azure-api.net/spicedelightappapi/api/Product", {
+        headers: {
+           Authorization: `Bearer ${token}`
+        }
+     });
 
-    console.log(response.data);
+     return { body: JSON.stringify(response) };
+
 }
-
 export async function server(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
 
     const name = request.query.get('name') || await request.text() || 'world';
-    await callApiManagement()
 
     return { body: `Hello, ${name}!` };
 };
@@ -28,4 +30,10 @@ app.http('server', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: server
+});
+
+app.http('product', {
+    methods: ['GET'],
+    authLevel: 'anonymous',
+    handler: callApiManagement
 });
